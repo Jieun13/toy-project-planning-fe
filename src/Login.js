b23dad45006b986320624bcd9965a1296bc4858d
jieun13 <ynb9598@gmail.com>
2025-02-11 22:11:27 +0900

    d2: oauth2 로그인 추가

--- src/Login.js
+++ src/Login.js
@@ -2,15 +2,24 @@ import React from 'react';
 
 const Login = () => {
     return (
-        <div>
+        <div style={{ textAlign: 'center' }}>
             <h1>로그인 페이지</h1>
-            <a href="http://localhost:8080/oauth2/authorization/kakao">
-                <img
-                    src="/kakaoLogin.png"
-                    alt="카카오 로그인"
-                    style={{ width: '150px', height: 'auto' }} // 버튼 크기 조정 가능
-                />
-            </a>
+            <div style={{ display: 'flex', justifyContent: 'center', gap: '20px' }}>
+                <a href="http://localhost:8080/oauth2/authorization/kakao">
+                    <img
+                        src="/kakaoLogin.png"
+                        alt="카카오 로그인"
+                        style={{ width: '150px', height: 'auto' }}
+                    />
+                </a>
+                <a href="http://localhost:8080/oauth2/authorization/google">
+                    <img
+                        src="/googleLogin.png"
+                        alt="구글 로그인"
+                        style={{ width: '150px', height: 'auto' }}
+                    />
+                </a>
+            </div>
         </div>
     );
 };
